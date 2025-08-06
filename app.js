ecd501a21b3419201ccd3d56b3a407de07c2da81
Gelson Ribeiro Júnior <junior.ar.ribeiro@gmail.com>
2025-08-06 09:06:39 -0300

    Primeira entrada dos nomes e da lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// array de amigos e variável para seu nome
+let amigos = [];
+let meuNome = '';
+
+// Entrada do seu próprio nome
+let definirMeuNome = () => {
+  let input = document.getElementById('meuNome');
+  let nome = input.value.trim();
+
+  if (nome === '') {
+    alert('Por favor, insira seu nome.');
+  } else {
+    meuNome = nome;
+    input.disabled = true;
+    document.getElementById('botaoMeuNome').disabled = true;
+  }
+};
+
+// 2) Adicionar amigos a lista
+let adicionarAmigo = () => {
+  let input = document.getElementById('amigo');
+  let nome = input.value.trim();
+
+  if (nome === '') {
+    alert('Por favor, insira um nome.');
+  } else {
+    amigos.push(nome);
+    input.value = '';
+    criarLista();
+  }
+};
+
